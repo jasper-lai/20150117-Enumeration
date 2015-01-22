f328f63bda924f93eeca90f657935a6dd234ed3d
jasper-lai <jasper.lai@gmail.com>
2015-01-22 16:20:00 +0800

    add IQueryable sample
    add IQueryable sample

--- EnumerationSample01/Program.cs
+++ EnumerationSample01/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EnumerationSample01
+namespace IQueryableSample01
 {
     class Program
     {
---
+++ IQueryableSample01/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq08_6_IQueryable
+{
+    class Program
+    {
+        
+        private static void TestIEnumerable()
+        {
+            using (NorthwindEntities ctx = new NorthwindEntities())
+            {
+                IEnumerable<Employee> emps = ctx.Employees;
+                int count = emps.Where(x => x.EmployeeID == 2).Count();
+
+                Console.WriteLine("Count={0}", count);
+            }
+        }
+
+        private static void TestIQueryable()
+        {
+            using (NorthwindEntities ctx = new NorthwindEntities())
+            {
+                IQueryable<Employee> emps = ctx.Employees;
+                int count = emps.Where(x => x.EmployeeID == 2).Count();
+
+                Console.WriteLine("Count={0}", count);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            TestIEnumerable();
+            TestIQueryable();
+
+            Console.ReadLine();
+        }
+    }
+}
